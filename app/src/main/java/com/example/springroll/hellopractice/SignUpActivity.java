f929bc657a7209ab96d380776ecfed92e546fff7
kevin <hoangja@plu.edu>
2015-11-22 00:08:04 -0800

    Initial commit

---
+++ app/src/main/java/com/example/springroll/hellopractice/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.springroll.hellopractice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by SpringRoll on 11/21/2015.
+ */
+public class SignUpActivity extends Activity {
+
+    @Override
+    protected  void onCreate(Bundle saveInstanceState){
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
